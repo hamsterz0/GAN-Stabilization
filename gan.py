34ade10197dcc091a33cb448b9c0c18a6a58e0d5
thearnavgarg <thearnavgarg@gmail.com>
2019-03-16 22:51:17 -0700

    Forgot to add the imports in GAN

--- gan.py
+++ gan.py
@@ -1,3 +1,17 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
