2d2a04a070be6c9b6b6d0593a2ac236a31059605
thearnavgarg <thearnavgarg@gmail.com>
2019-03-16 23:54:18 -0700

    added the reparamerterization layer

--- gan.py
+++ gan.py
@@ -11,6 +11,7 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+from layers import SVDConv2d
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -63,27 +64,28 @@ class Generator(nn.Module):
         return output
 
 class Discriminator(nn.Module):
-    def __init__(self, ngpu, nz, ndf, nc):
+    def __init__(self, ngpu, nz, ndf, nc, k):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
+        self.k = k
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            SVDConv2d(nc, ndf, 4, k=self.k, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            SVDConv2d(ndf, ndf * 2, 4,k=self.k,  2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            SVDConv2d(ndf * 2, ndf * 4, 4,k=self.k,  2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            SVDConv2d(ndf * 4, ndf * 8, 4,k=self.k,  2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            SVDConv2d(ndf * 8, 1, 4,k=self.k,  1, 0, bias=False),
             nn.Sigmoid()
         )
 
